d9d473c8d7e0a1fdfd00f12df19f63747c2c7c19
Fabio Frasson <fabio.frass@gmail.com>
2022-06-26 19:55:20 -0300

    final first app backend setup

--- src/alpha.js
+++ src/alpha.js
@@ -6,6 +6,30 @@ const alphaPath = new alphaDb("./src/databases/alpha.db");
 const httpServer = http.createServer();
 const websocketServer = new WebSocketServer({ server: httpServer });
 
+websocketServer.on("connection", (socket) => {
+ // parâmetro socket da callback é a própria conexão
+ socket.on("message", (message) => {
+  saveDataToDb(convertMessageToJson(message));
+ })
+})
+
+function convertMessageToJson(buffer) {
+ const message = buffer.toString();
+ return JSON.parse(message);
+}
+
+const insertQuery = "INSERT INTO coordinate (LATITUDE, LONGITUDE, COMMENT) VALUES (?, ?, ?)";
+
+function saveDataToDb(data) {
+ try {
+  alphaPath.run(insertQuery, [data.latitude, data.longitude, data.comment], (err) => {
+   console.log(err)
+  })
+ } catch (err) {
+  console.log(err)
+ }
+}
+
 httpServer.once("listening", () => {
 
  const query = "CREATE TABLE IF NOT EXISTS coordinate(ID INT AUTO INCREMENT PRIMARY KEY, LATITUDE VARCHAR NOT NULL, LONGITUDE VARCHAR NOT NULL, COMMENT VARCHAR(512) NOT NULL)";
