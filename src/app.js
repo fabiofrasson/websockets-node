703f6ef83d35aa0202322235acb727644fcc854a
Fabio Frasson <fabio.frass@gmail.com>
2022-06-26 18:59:15 -0300

    http server setup and database basic query instruction

--- src/app.js
+++ src/app.js
@@ -4,3 +4,20 @@ const sqlite3 = require("sqlite3");
 const alphaDb = sqlite3.verbose().Database;
 const alphaPath = new alphaDb("./src/databases/alpha.db");
 const httpServer = http.createServer();
+const websocketServer = new WebSocketServer({ server: httpServer });
+
+httpServer.once("listening", () => {
+
+ const query = "CREATE TABLE IF NOT EXISTS coordinate(ID INT AUTO INCREMENT PRIMARY KEY, LATITUDE VARCHAR NOT NULL, LONGITUDE VARCHAR NOT NULL, COMMENT VARCHAR(512) NOT NULL)";
+
+ alphaPath.exec(query, (err) => {
+  if (err) {
+   console.log(err);
+  }
+  console.log("Your shitty query was executed, loser!")
+ });
+})
+
+httpServer.listen(8090, () => {
+ console.log("Server is fucking running! =D")
+});
